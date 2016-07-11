9606020ef18df7111f94df56c7393eaceeec0951
silk10268 <silk10268@gmail.com>
2016-07-11 23:36:33 +0800

    第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190，測試通過，綠燈

--- PotterShoppingCart/PotterShoppingCart/BookStore.cs
+++ PotterShoppingCart/PotterShoppingCart/BookStore.cs
@@ -7,14 +7,20 @@ namespace BookStore
 {
     public class BookStore
     {
+        public Dictionary<int,double> Discount { get; set; }
+
         public BookStore()
         {
+            Discount = new Dictionary<int, double> { };
+            Discount[1] = 1.0;
+            Discount[2] = 0.95;
 
         }
 
-        public int CalculatePrice(ShoppingCart shoppingCart)
+        public double CalculatePrice(ShoppingCart shoppingCart)
         {
-            return shoppingCart.BookCount.Sum((x) => x.Book.Price * x.Count);
+            int episodeCount = shoppingCart.BookCount.Select((x) => x.Book.Episode).Distinct().Count();
+            return shoppingCart.BookCount.Sum((x) => x.Book.Price * x.Count) * Discount[episodeCount];
         }
 
     }
