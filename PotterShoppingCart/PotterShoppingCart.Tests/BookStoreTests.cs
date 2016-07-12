fca521b6332ea3c126b691baec914744209a5cdc
silk10268 <silk10268@gmail.com>
2016-07-12 08:11:51 +0800

    Scenario: 一二三集各買了一本，價格應為100*3*0.9=270，測試未通過，紅燈

--- PotterShoppingCart/PotterShoppingCart.Tests/BookStoreTests.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/BookStoreTests.cs
@@ -41,5 +41,26 @@ namespace BookStore.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CalculatePriceTest_Buy_Episode_1_2_3_Price_270()
+        {
+            //arrange
+            var bookStore = new BookStore();
+            var shoppingCart = new ShoppingCart();
+            var bookCount = new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1);
+            shoppingCart.AddBookCounts(new List<BookCount>
+                {
+                new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1),
+                new BookCount(new Book { Name = "Potter", Episode = 2, Price = 100 }, 1),
+                new BookCount(new Book { Name = "Potter", Episode = 3, Price = 100 }, 1)
+                    });
+
+            //act
+            var actual = bookStore.CalculatePrice(shoppingCart);
+            var expected = 270;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
