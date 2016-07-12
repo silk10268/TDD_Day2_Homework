83b95aacbee699ede35139bea73bab8bcc54ba9d
silk10268 <silk10268@gmail.com>
2016-07-12 10:09:36 +0800

    Scenario: 第一集買了一本，第二三集各買了兩本，價格應為100*3*0.9 + 100*2*0.95 = 460，綠燈

--- PotterShoppingCart/PotterShoppingCart.Tests/BookStoreTests.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/BookStoreTests.cs
@@ -122,5 +122,25 @@ namespace BookStore.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CalculatePriceTest_Buy_Episode_1_2_2_3_3_Price_460()
+        {
+            //arrange
+            var bookStore = new BookStore();
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddBookCounts(new List<BookCount>
+                {
+                new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1),
+                new BookCount(new Book { Name = "Potter", Episode = 2, Price = 100 }, 2),
+                new BookCount(new Book { Name = "Potter", Episode = 3, Price = 100 }, 2)
+                    });
+
+            //act
+            var actual = bookStore.CalculatePrice(shoppingCart);
+            var expected = 460;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
