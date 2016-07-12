a212b1d9035aad1968a7caa0de199721400884be
silk10268 <silk10268@gmail.com>
2016-07-12 08:15:09 +0800

    Scenario: 一二三四集各買了一本，價格應為100*4*0.8=320，測試未通過，紅燈

--- PotterShoppingCart/PotterShoppingCart.Tests/BookStoreTests.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/BookStoreTests.cs
@@ -28,7 +28,6 @@ namespace BookStore.Tests
             //arrange
             var bookStore = new BookStore();
             var shoppingCart = new ShoppingCart();
-            var bookCount = new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1);
             shoppingCart.AddBookCounts(new List<BookCount>
                 {
                 new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1),
@@ -48,12 +47,32 @@ namespace BookStore.Tests
             //arrange
             var bookStore = new BookStore();
             var shoppingCart = new ShoppingCart();
-            var bookCount = new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1);
             shoppingCart.AddBookCounts(new List<BookCount>
                 {
                 new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1),
                 new BookCount(new Book { Name = "Potter", Episode = 2, Price = 100 }, 1),
                 new BookCount(new Book { Name = "Potter", Episode = 3, Price = 100 }, 1)
+                    });
+
+            //act
+            var actual = bookStore.CalculatePrice(shoppingCart);
+            var expected = 270;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePriceTest_Buy_Episode_1_2_3_4_Price_320()
+        {
+            //arrange
+            var bookStore = new BookStore();
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddBookCounts(new List<BookCount>
+                {
+                new BookCount(new Book { Name = "Potter", Episode = 1, Price = 100 }, 1),
+                new BookCount(new Book { Name = "Potter", Episode = 2, Price = 100 }, 1),
+                new BookCount(new Book { Name = "Potter", Episode = 3, Price = 100 }, 1),
+                new BookCount(new Book { Name = "Potter", Episode = 4, Price = 100 }, 1)
                     });
 
             //act
